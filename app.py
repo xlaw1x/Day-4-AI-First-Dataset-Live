7126eef4d14e675038fce6a80c6f7efe72ded18a
Leonard Ray Inciso <149936342+xlaw1x@users.noreply.github.com>
2024-11-15 20:10:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ Response: "Fish shipments like bangus require strict temperature control and are
          if 'messagess' not in st.session_state:
              st.session_state.messagess = []
              st.session_state.messagess.append({"role": "system", "content": System_Prompt})
-             chat =  openai.ChatCompletion.create(model = "gpt-4o-mini", messages = st.session_state.messagess, temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
+             chat =  openai.ChatCompletion.create(model = "gpt-4o-mini", messages = st.session_state.messagess, temperature=0.2, max_tokens=500, top_p=1, frequency_penalty=0, presence_penalty=0)
              response = chat.choices[0].message.content
              st.session_state.messagess.append({"role": "assistant", "content": response})
 
